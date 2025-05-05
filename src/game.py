4bc042a219466e6f1c856024fdbfdf8cd286800d
Krzysztof Kulka <krzysztof.kulka1234@gmail.com>
2025-05-05 17:31:24 +0200

    refactoring and raport in ipynb

--- src/game.py
+++ src/game.py
@@ -19,9 +19,10 @@ class ClobberAgent:
         print(f"{self.name} is playing...")
         print(f"Current board:\n{game.board}")
         print(f"Current player: {game.current_player}")
-        # print(f"Possible moves: {game.get_possible_moves()}")
         print(f"Evaluating moves using {self.strategy} strategy...")
+
         dt=DecisionTree(self.max_depth, copy.deepcopy(game), self.heuristic, self.strategy, self.name)
+
         if self.adaptive:
             potential_new_heuristic = dt.analyze_and_change_heuristic(copy.deepcopy(game))
             self.heuristic= potential_new_heuristic if potential_new_heuristic else self.heuristic
